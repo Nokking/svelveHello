891983badf8725c8c28e62cc1b760b876761cadf
Enosk <japaneseplay1994@gmail.com>
2022-01-13 06:58:02 +0000

    created store

---
+++ {training-svelte}/src/components/Header.svelte
@@ -0,0 +1,18 @@
+<header class="header">
+    <a href="/" class="header_logo">
+      <img src="img/logo.png" alt="LOGO">
+    </a>
+  </header>
+  
+  <style>
+  .header {
+    width: 100%;
+    background-color: #fff;
+    padding: 20px 0;
+  }
+  .header_logo {
+    width: 50px;
+    display: block;
+    margin: 0 auto;
+  }
+  </style>
---
+++ {training-svelte}/src/components/TodoForm.svelte
@@ -0,0 +1,40 @@
+<script>
+    import { todos } from '../stores/todo'
+
+    let text = ''
+    let errorMessage = ''
+
+    const registerTodo = () => {
+        if(text === ''){
+            errorMessage = 'Text not found.'
+            return
+        }
+        errorMessage = ''
+        const id = $todos.length + 1
+        todos.update((todo)=> {
+            return [...todo, {id, text, 'isDone': false}]
+        })
+
+        text = ''
+    }
+
+</script>
+
+<div class="main_form">
+    {#if errorMessage}
+        <p class="main_form_error">{errorMessage}</p>    
+    {/if}
+    <form class="main_form_body">
+        <input 
+            bind:value={text} 
+            class="main_form_txt" 
+            type="text" 
+            placeholder="TODO">
+        <button 
+            on:click={registerTodo} 
+            class="main_form_submit" 
+            type="button">
+        追加
+        </button>
+    </form>
+</div>
