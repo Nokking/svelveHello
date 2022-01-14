ad47b09cfc59b28866cb978b01e4d0f6aeab7bfa
Enosk <japaneseplay1994@gmail.com>
2022-01-14 01:12:52 +0000

    changed App.svelte

--- {training-svelte}/src/App.svelte
+++ {training-svelte}/src/App.svelte
@@ -1,30 +1,14 @@
 <script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+	import Header from './components/Header.svelte';
+	import TodoForm from './components/TodoForm.svelte';
+	import TodoContents from './components/TodoContents.svelte';
+  </script>
+  
+  <Header />
+  <main class="main">
+	<TodoForm />
+	<TodoContents />
+  </main>
+  
+  <style>
+  </style>
